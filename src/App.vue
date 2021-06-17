f8f28c612ea0699cd30612fc39d15ea21fcd9d57
abc820219 <z27089433@gmail.com>
2021-06-17 19:53:19 +0800

    遞迴組件練習

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,33 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <ul class="menu">
+      <MenuItem :menu-list="menuList" />
+    </ul>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+import MenuItem from './MenuItem.vue'
+export default {
+  components: {
+    MenuItem
+  },
+  data () {
+    return {
+      menuList: {
+        label: '24購物',
+        children: [
+          {
+            label: '桌由',
+            children: [
+              {
+                label: '好玩'
+              }
+            ]
+          }
+        ]
+      }
     }
   }
 }
-</style>
+</script>
---
+++ src/MenuItem.vue
@@ -0,0 +1,45 @@
+<template>
+  <li>
+    <span
+      v-if="menuList.children && menuList.children.length"
+      @click="openHandler"
+    > {{ open? '-': '+' }}</span>
+    {{ menuList.label }}
+    <ul v-show="open">
+      <MenuItem
+        v-for="list in menuList.children"
+        :key="
+          list.label"
+        :menu-list="list"
+      />
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'MenuItem',
+  props: {
+    menuList: {
+      type: Object,
+      default () {
+        return {}
+      }
+    }
+  },
+  data () {
+    return {
+      open: false
+    }
+  },
+  methods: {
+    openHandler () {
+      this.open = !this.open
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
