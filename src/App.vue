cfe11e5a54421d9a25683243722fa0908d78cb71
abc820219 <z27089433@gmail.com>
2021-10-11 13:34:08 +0800

    抽屜組件

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,46 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <Drawer v-model="visible" :position="position" width="100%" height="300px">content</Drawer>
+    <Drawer v-model="visible1" :position="position" width="200px" height="100%">content</Drawer>
+    <button @click="visible = true">button</button>
+    <button @click="l">l</button>
+    <button @click="r">r</button>
+    <button @click="t">t</button>
+    <button @click="b">b</button>
   </div>
 </template>
-
+<script>
+import Drawer from "@/components/Drawer";
+export default {
+  components: {
+    Drawer,
+  },
+  data() {
+    return {
+      visible: false,
+      visible1: false,
+      position: "top",
+    };
+  },
+  methods: {
+    l() {
+      this.position = "left";
+      this.visible1 = true;
+    },
+    r() {
+      this.position = "right";
+      this.visible1 = true;
+    },
+    t() {
+      this.position = "top";
+      this.visible = true;
+    },
+    b() {
+      this.position = "bottom";
+      this.visible = true;
+    },
+  },
+};
+</script>
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
---
+++ src/components/Drawer.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="drawer" v-if="value" @click="$emit('input', false)">
+    <div
+      @click.stop
+      class="content"
+      :style="{ width, height }"
+      :class="position"
+    >
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: Boolean,
+      default: false,
+    },
+    width: {
+      type: String,
+      default: "300px",
+    },
+    height: {
+      type: String,
+      default: "300px",
+    },
+    position: {
+      type: String,
+      default: "top",
+    },
+  },
+  data() {},
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.drawer {
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.3);
+  z-index: 9999;
+  display: flex;
+  .content {
+    position: absolute;
+    background-color: #fff;
+    &.left {
+      left: 0;
+      top: 0;
+      bottom: 0;
+    }
+    &.right {
+      right: 0;
+      top: 0;
+      bottom: 0;
+    }
+    &.bottom {
+      bottom: 0;
+      left: 0;
+      right: 0;
+    }
+    &.top {
+      top: 0;
+      left: 0;
+      right: 0;
+    }
+  }
+}
+</style>
